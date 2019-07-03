d43b90fca8d8a4572a38553d7a327973cbd4f492
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-03 22:08:36 +0300

    Complete UserItem

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -13,14 +13,21 @@ export class UserItem extends Component {
 	}
 
 	render() {
+		const { avatar_url, login, html_url } = this.state;
 		return (
 			<div className='card text-center'>
 				<img
-					src={this.state.avatar_url}
+					src={avatar_url}
 					alt=''
 					className='round-img'
 					style={{ width: 60 }}
 				/>
+				<h3>{login}</h3>
+				<div>
+					<a href={html_url} className='btn btn-dark btn-sm my-1'>
+						More
+					</a>
+				</div>
 			</div>
 		);
 	}
