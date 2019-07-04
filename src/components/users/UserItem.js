34e199cb9a7a668b07883de04cb8b937501c0947
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-04 11:42:39 +0300

    Fix PropTypes

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import propTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 	return (
@@ -21,7 +21,7 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 };
 
 UserItem.propTypes = {
-	user: propTypes.object.isRequired
+	user: PropTypes.object.isRequired
 };
 
 export default UserItem;
