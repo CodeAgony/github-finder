e67026ec928854ab319050ffbeed3b8a626776fe
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-04 11:22:47 +0300

    Rewrite as functional component

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,30 +1,28 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
+import propTypes from 'prop-types';
 
-export class UserItem extends Component {
-	static propTypes = {
-		user: PropTypes.object.isRequired
-	};
-
-	render() {
-		const { login, avatar_url, html_url } = this.props.user;
-		return (
-			<div className='card text-center'>
-				<img
-					src={avatar_url}
-					alt=''
-					className='round-img'
-					style={{ width: 60 }}
-				/>
-				<h3>{login}</h3>
-				<div>
-					<a href={html_url} className='btn btn-dark btn-sm my-1'>
-						More
-					</a>
-				</div>
+const UserItem = props => {
+	const { login, avatar_url, html_url } = props.user;
+	return (
+		<div className='card text-center'>
+			<img
+				src={avatar_url}
+				alt=''
+				className='round-img'
+				style={{ width: 60 }}
+			/>
+			<h3>{login}</h3>
+			<div>
+				<a href={html_url} className='btn btn-dark btn-sm my-1'>
+					More
+				</a>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+
+UserItem.propTypes = {
+	user: propTypes.object.isRequired
+};
 
 export default UserItem;
