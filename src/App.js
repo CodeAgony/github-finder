7e0b8f6fecca7cba5d9950823e9335c96e86fda0
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-06 23:07:26 +0300

    Bring in User component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+/* eslint-disable react/jsx-no-duplicate-props */
 import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import User from './components/users/User';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
@@ -11,6 +13,7 @@ import About from './components/pages/About';
 class App extends Component {
 	state = {
 		users: [],
+		user: {},
 		loading: false,
 		alert: null
 	};
@@ -33,6 +36,19 @@ class App extends Component {
 	// Clear API response data from state
 	clearUsers = () => this.setState({ users: [], loading: false });
 
+	// Get single user
+	getUser = async username => {
+		this.setState({ loading: true });
+
+		const res = await axios.get(
+			`https://api.github.com/users=${username}?client_id=${
+				process.env.REACT_APP_GITHUB_CLIENT_ID
+			}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({ user: res.data, loading: false });
+	};
+
 	// Lift alert state
 	setAlert = (msg, type) => {
 		this.setState({ alert: { msg, type } });
@@ -42,7 +58,7 @@ class App extends Component {
 	};
 
 	render() {
-		const { users, loading } = this.state;
+		const { users, user, loading } = this.state;
 
 		return (
 			<Router>
@@ -67,6 +83,16 @@ class App extends Component {
 								)}
 							/>
 							<Route exact path='/about' component={About} />
+							<Route
+								exact
+								path='/user:login'
+								render={props => (
+									<Fragment>
+										<User {...props} getUser={this.getUser} user={user} />
+										<Users loading={loading} users={users} loading={loading} />
+									</Fragment>
+								)}
+							/>
 						</Switch>
 					</div>
 				</div>
