4f2367841b85d0c4dad1891e1be724249b617dfb
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-06 19:48:38 +0300

    Add comments

--- src/App.js
+++ src/App.js
@@ -13,22 +13,30 @@ class App extends Component {
 		alert: null
 	};
 
+	// Get users from API based on search query
 	searchUsers = async text => {
+
+		// Show the loading spinner while data is on the way
 		this.setState({ loading: true });
+		
 		const res = await axios.get(
 			`https://api.github.com/search/users?q=${text}&client_id=${
 				process.env.REACT_APP_GITHUB_CLIENT_ID
 			}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
+
+		// Store response in state and remove spinner
 		this.setState({ users: res.data.items, loading: false });
 	};
 
+	// Clear API response data from state
 	clearUsers = () => this.setState({ users: [], loading: false });
 
+	// Lift alert state
 	setAlert = (msg, type) => {
 		this.setState({ alert: { msg, type } });
 
-		//Remove alert in 5 seconds
+		// Remove alert in 5 seconds
 		setTimeout(() => this.setState({ alert: null }), 5000);
 	};
 
