060838ec338d14e0f3a0a099e5f2f2c4959929f8
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-06 07:46:43 +0300

    Update state onChange

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
 export class Search extends Component {
+	state = {
+		text: ''
+	};
+
+	onChange = e => this.setState({ [e.target.name]: e.target.value });
+
 	render() {
 		return (
 			<div>
 				<form className='form'>
-					<input type='text' name='text' placeholder='Search users...' />
+					<input
+						type='text'
+						name='text'
+						placeholder='Search users...'
+						onChange={this.onChange}
+					/>
 					<input
 						type='submit'
 						value='Search'
