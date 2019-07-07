e9f5186d6d59a1761ed1c5ef8414e2719046d0c8
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-07 16:37:54 +0300

    Add API call logic

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,8 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 import React, { Component } from 'react';
 
 export class User extends Component {
+	componentDidMount() {
+		this.props.getUser(this.props.match.params.login);
+	}
 	render() {
 		return <div>User</div>;
+		return <div> test </div>;
 	}
 }
 
