1ef585b962925d25818e586c214af57a20626c46
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-07 22:02:52 +0300

    Render out the rest of data

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -11,7 +11,18 @@ export class User extends Component {
 	}
 	render() {
 		const {
+			name,
+			company,
 			avatar_url,
+			location,
+			bio,
+			blog,
+			login,
+			html_url,
+			followers,
+			following,
+			public_repos,
+			public_gists,
 			hireable
 		} = this.props.user;
 
@@ -38,7 +49,46 @@ export class User extends Component {
 							alt=''
 							style={{ width: '150px' }}
 						/>
+						<h1>{name}</h1>
+						<p>Location: {location}</p>
 					</div>
+					<div>
+						{bio && (
+							<Fragment>
+								<h3>Bio</h3>
+								<p>{bio}</p>
+							</Fragment>
+						)}
+						<a href={html_url} className='btn btn-dark my-1'>
+							Visit Github Profile
+						</a>
+						<ul>
+							<li>
+								{login && (
+									<Fragment>
+										<strong>Username: </strong> {login}
+									</Fragment>
+								)}
+							</li>
+
+							<li>
+								{company && (
+									<Fragment>
+										<strong>Company: </strong> {company}
+									</Fragment>
+								)}
+							</li>
+
+							<li>
+								{blog && (
+									<Fragment>
+										<strong>Website: </strong> {blog}
+									</Fragment>
+								)}
+							</li>
+						</ul>
+					</div>
+				</div>
 				</div>
 			</Fragment>
 		);
