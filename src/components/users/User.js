452a7cad099b5e68f5a915d78341dcf816c9b9a8
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-07 20:34:37 +0300

    Add hireable marker

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -10,6 +10,10 @@ export class User extends Component {
 		this.props.getUser(this.props.match.params.login);
 	}
 	render() {
+		const {
+			hireable
+		} = this.props.user;
+
 		const { loading } = this.props;
 
 		if (loading) return <Spinner />;
@@ -19,6 +23,12 @@ export class User extends Component {
 				<Link to='/' className='btn btn-light'>
 					Back to Search
 				</Link>
+				Hireable:{' '}
+				{hireable ? (
+					<i className='fas fa-check text-success' />
+				) : (
+					<i className='fas fa-times-circle text-danger' />
+				)}
 			</Fragment>
 		);
 	}
