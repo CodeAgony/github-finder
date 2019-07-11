7e5b4b7905cd77adb67b78c76a88c795289d465f
CodeAgony <yaroslav.bkh@gmail.com>
2019-07-11 14:22:06 +0300

    Add home link

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
 	return (
 		<div>
 			<h1>Not Found</h1>
 			<p className='lead'>The page you are lloking for does not exist</p>
+			<Link to='/'>Return to the home page</Link>
 		</div>
 	);
 };
